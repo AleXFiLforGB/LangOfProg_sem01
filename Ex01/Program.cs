f2e8ecfff8fe41c2f78ed170c75ba6bef2c61171
Филоненко Александр <alcohealer@gmail.com>
2021-11-02 15:03:33 +0300

    add Ex0q

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,1 +1,69 @@
-﻿Console.WriteLine("Hello, Kitty!");
+﻿// при построении вопроса следуют придерживаться следующего алгоритма 
+// 1. Какая задача - описать выполняемую задачу 
+// 2. Я делаю то и то - описание действий 
+// 3. Какие данные используются 
+// 4. Что вы ожидаете - описание желаемого результата 
+// 5. Что получается в итоге 
+// Ошибки 
+// почему? как исправить? 
+
+// типы данных int double string bool 
+
+// Переменная 
+// if -else 
+// while 
+
+// поменять 2 переменные местами 
+
+// int a = 1; 
+// int b = 3;
+
+// Console.Write(" a = ");
+// Console.Write(a);
+// Console.Write(" b = ");
+// Console.WriteLine(b);
+
+// int temp = a;
+// a = b;
+// b = temp;
+
+// Console.Write(" a = ");
+// Console.Write(a);
+// Console.Write(" b = ");
+// Console.WriteLine(b);
+
+// ==
+
+// Console.WriteLine(" a = " + a + " b = " + b);
+
+// Console.WriteLine("a = {0} b = {1}", a, b);
+
+
+// Console.WriteLine($"a = {a} b = {b}");
+
+
+// Переменная 
+// Тип данных 
+// Имя
+// Значение
+
+// int a = new Random().Next(1, 100); 
+// int b = new Random().Next(1,100);
+// Console.WriteLine($"a = {a} b = {b}");
+
+// if (a > b)
+// {
+//     Console.WriteLine(a);
+// }
+
+// else 
+// {
+//     Console.WriteLine(b);
+// }
+
+int current = 1; 
+ while (current <= 10)
+ {
+     Console.WriteLine($"{current} {current*current}");
+     current++;
+ }
---
+++ Ex03/Program.cs
@@ -0,0 +1,42 @@
+﻿// Показать все числа на [2;128]
+// которые делятся на 3 и 5 
+// bool f = (88 % 22) == 0;
+// Console.WriteLine(88 % 22 == 0);
+
+// int start = 2;
+// int end = 128;
+// int current = start;
+// int count = 1;
+
+// while (current <= end)
+// {
+//     if ((current % 3 == 0) && (current % 5 == 0))
+//     {
+//         Console.WriteLine($"{count}) {current}");
+//         count++;
+//     }
+//     current++;
+// }
+
+
+int candy = new Random().Next(20,39); //кол-во конфет
+int max = 5; 
+int player = 1;
+
+while (candy > 0)
+{
+    Console.WriteLine($"Количество конфет: {candy}");
+    Console.WriteLine($"Игрок {player} берет конфеты");
+    Console.Write("Введите количество конфет, которые вы хотите отобрать ");
+    int count = 0;
+    while (!(count >=1 && count <= max))
+    {
+        count = int.Parse (Console.ReadLine());
+        Console.WriteLine("Можно взять только от 1 до 5 конфет");
+    }
+    candy=candy-count;
+    if (candy>0)
+    {if (player==1){player=2;} else player=1;}
+}
+Console.WriteLine($"Победил игрок {player}");
+
